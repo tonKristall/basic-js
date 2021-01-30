974acf477a0752ca48f8eff30cde109ffc279274
tonKristall <krisale@yandex.ru>
2021-01-30 15:32:18 +0300

    fix: rename var

--- src/count-cats.js
+++ src/count-cats.js
@@ -5,7 +5,7 @@ module.exports = function countCats(matrix) {
   for (i = 0; i < matrix.length; i++) {
     for (j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] == "^^") {
-        n++;
+        cats++;
       }
     }
   }
