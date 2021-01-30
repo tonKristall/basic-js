37baeff8db38220c874ce66d7dadf57325702bf0
tonKristall <krisale@yandex.ru>
2021-01-31 00:04:27 +0300

    feat: carbon pass

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity) {
+    return false;
+  }
+  if (sampleActivity * 1 !== 0) {
+    return false;
+  }
+  if ((sampleActivity > 0) && (sampleActivity <= 50)) {
+    let appr = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD / Math.log(2));
+    console.log(appr);
+    return appr;
+  } else {
+    return false;
+  }
 };
