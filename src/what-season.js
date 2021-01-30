acd9eca41edfc95885c83ffb229608da3106d741
tonKristall <krisale@yandex.ru>
2021-01-30 22:16:27 +0300

    feat: seasson pass

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) {
+    return ("Unable to determine the time of year!");
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error();
+  }
+  const seasons = [
+    "winter",
+    "winter",
+    "spring",
+    "spring",
+    "spring",
+    "summer",
+    "summer",
+    "summer",
+    "autumn",
+    "autumn",
+    "autumn",
+    "winter",
+  ]
+  let m = date.getMonth();
+  return seasons[m];
+}
