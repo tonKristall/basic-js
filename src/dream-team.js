746a024186c3b00d59f11cdf0269ca635ebe3e20
tonKristall <krisale@yandex.ru>
2021-01-30 17:51:17 +0300

    feat: team pass

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!members) {
+    return false;
+  }
+  let name = "";
+  let massletters = [];
+  for (i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      let letters = members[i].trim().split("");
+      massletters.push(letters[0].toUpperCase());
+    }
+  }
+  massletters.sort();
+  for (i = 0; i < massletters.length; i++) {
+    name = name + (massletters[i]);
+  }
+  return name;
+}
