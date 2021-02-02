3b92acb7b985667fc756608f5cde30b1245cdcbb
tonKristall <krisale@yandex.ru>
2021-02-02 23:39:58 +0300

    feat: task hanoi pass

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = 0;
+  for (i = 1; i < disksNumber + 1; i++) {
+    turns = turns * 2 + 1;
+  }
+  let seconds = Math.trunc(turns * 3600 / turnsSpeed);
+  return { turns, seconds };
+}
