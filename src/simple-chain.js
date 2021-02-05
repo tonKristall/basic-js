4bd845112e2f3ff840f7a7734c0ce3414c940e93
tonKristall <krisale@yandex.ru>
2021-02-06 00:41:14 +0300

    feat: task chain pass

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(String(value));
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ((position > 0) && (position <= this.chain.length)) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
+    this.chain = [];
+    throw new Error();
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.join(" )~~( ");
+    this.chain = [];
+    return ("( " + result + " )");
   }
 };
 
